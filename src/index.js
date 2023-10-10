24b811f951664e21a10f587c1a537cdbd9d259ae
Eider22 <eidercardona22@gmail.com>
2023-10-09 21:50:13 -0500

    Se establece la estructura de las quintuplas que componen un autómata

--- src/index.js
+++ src/index.js
@@ -1,28 +1,51 @@
+import { union } from "./scripts/operations/union.js";
 import { addState, getStates } from "./scripts/states.js";
 
 const d = document;
 
+const data = {
+    inptuStateSelector: "inputState-0",
+    counter: 0,
+}
+
 d.addEventListener('DOMContentLoaded', () => {
-
     const buttonStateSelector = "#buttonState";
-    const inptuStateSelector = "#inputState";
-
-    console.log("hello")
-    events.captureInput(buttonStateSelector, inptuStateSelector);    
-    // addState
+    
+    events.captureInput(buttonStateSelector);
 })
 
 
 
 
 const events = {
-    captureInput: (buttonSelector, inputSelector) => {
+    captureInput: (buttonSelector) => {
         const buttonStateElement = d.querySelector(buttonSelector);
-        const inputStateElement = d.querySelector(inputSelector);
 
         buttonStateElement.addEventListener('click', (e) => {
-            addState(inputStateElement.value);
-            console.log(getStates())
+            let inputStateElement = d.querySelector("#" + data.inptuStateSelector);
+            if (inputStateElement.value) {
+                if (addState(inputStateElement.value)) {
+                    data.counter++;
+                    const splitElements = data.inptuStateSelector.split('-');
+                    data.inptuStateSelector = splitElements[0] + "-" + data.counter;
+
+                    
+                    inputStateElement.disabled = true;
+                    factory.createInputState();
+                    console.log(getStates())
+                }
+
+            }
         })
+    }
+}
+
+
+const factory = {
+    createInputState: () => {
+        const input = d.createElement("input")
+        input.id = data.inptuStateSelector;
+        const inputStateButtonStateWraper = d.querySelector(".inputState-buttonState-wraper");
+        inputStateButtonStateWraper.appendChild(input);
     }
 }
--- src/scripts/states.js
+++ src/scripts/states.js
@@ -4,12 +4,12 @@ export const addState = (newState) => {
     const stateExist = states.find((state) => state === newState);
     if (stateExist) {
         console.log("El estado ya existe")
-        return;
+        return false;
     }
     states.push(newState);
-
+    return true;
 }
 
-export const getStates = (newState) => {
+export const getStates = () => {
     return states;
 }
